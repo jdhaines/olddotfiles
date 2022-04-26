f590c40359c2b93371c8e81b49b431375a3e97e9
Josh Haines <Josh@JoshHaines.com>
2022-04-26 11:17:17 -0400

    fix(install): add fish update

--- install.sh
+++ install.sh
@@ -2,18 +2,26 @@
 
 fishrc() {
 	echo "=============================================="
-	echo "            Copying Fish Config               "
+	echo "            Update Fish Install               "
 	echo "----------------------------------------------"
-	cp -Trv .config/ $HOME/.config/
+		sudo add-apt-repository ppa:fish-shell/release-3
+		sudo apt-get update
+		sudo apt-get install fish
 	echo "=============================================="
 	echo "            Install Fisher Manager            "
 	echo "----------------------------------------------"
-	curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+		curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 	echo "=============================================="
 	echo "            Install Plugins                   "
 	echo "----------------------------------------------"
-	fisher install rkbk60/onedark-fish
-	fisher install IlanCosman/tide@v5
+		fisher install IlanCosman/tide@v5
+		fisher install rkbk60/onedark-fish
+	echo "=============================================="
+	echo "            Copying Fish Config               "
+	echo "----------------------------------------------"
+		cp -Trv .config/ $HOME/.config/
+	echo "=============================================="
+	echo "            Done!                             "
 	echo "=============================================="
 }
 
