505900c3e5865a151901714174df541fbd75c49d
Josh Haines <Josh@JoshHaines.com>
2023-02-09 13:26:47 -0500

    fix(fish & fonts): add fish updates and fonts to dotfiles

--- install.sh
+++ install.sh
@@ -1,27 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 fishrc() {
-	echo "=============================================="
-	echo "            Update Fish Install               "
-	echo "----------------------------------------------"
 		sudo add-apt-repository ppa:fish-shell/release-3
 		sudo apt-get update
 		sudo apt-get -y install fish
-	echo "=============================================="
-	echo "            Install Fisher Manager            "
-	echo "----------------------------------------------"
-		#curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
     fish -c "curl -sL --insecure https://git.io/fisher | source && fisher install jorgebucaran/fisher"
-	echo "=============================================="
-	echo "            Install Plugins                   "
-	echo "----------------------------------------------"
-		#fisher install IlanCosman/tide@v5
-		#fisher install rkbk60/onedark-fish
     fish -c "fisher install IlanCosman/tide@v5"
     fish -c "fisher install jorgebucaran/nvm.fish"
-	echo "=============================================="
-	echo "            Copying Fish Config               "
-	echo "----------------------------------------------"
 		#cp -Trv .config/ $HOME/.config/
 		#rm $HOME/.config/fish/conf.d/omf.fish
     fish -c "echo 1 2 1 1 2 2 y | tide configure >/dev/null"
@@ -29,10 +14,13 @@ fishrc() {
     command -v fish | sudo tee -a /etc/shells
     # use fish as default shell
     sudo chsh -s $(which fish) $(whoami)
-	echo "=============================================="
-	echo "            Done!                             "
-	echo "=============================================="
 }
 
 fishrc
+
+# Fonts
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Jet Brains Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+fc-cache -f -v # rebuild font cache
+
 
